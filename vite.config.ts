57da884bdc381d43218a4aa033283cd3e08c6cf4
MitieSoft <info@mitiesoft.com>
2025-11-19 17:27:38 +0500

    Code upload

--- vite.config.ts
+++ vite.config.ts
@@ -43,9 +43,6 @@ export default defineConfig(async () => {
     build: {
       outDir: path.resolve(import.meta.dirname, "dist/public"),
       emptyOutDir: true,
-      rollupOptions: {
-        input: path.resolve(import.meta.dirname, "index.html"),
-      },
     },
     server: {
       fs: {
